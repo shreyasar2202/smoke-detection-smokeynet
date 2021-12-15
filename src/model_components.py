ef1b1229de051b63034a2f1cbfad1540d1d8b779
“Anshuman <adewanga@ucsd.edu>
2021-12-15 01:31:36 +0000

    Undo comments in model components

--- src/model_components.py
+++ src/model_components.py
@@ -26,18 +26,17 @@ import torchvision.models.detection
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from torchvision.models.detection.anchor_utils import AnchorGenerator
+from gtrxl_torch.gtrxl_torch import GTrXL
 
 # Other imports 
 import numpy as np
 import math
 
 # File imports
-# import resnet
+import resnet
 import util_fns
-# from rcnn import faster_rcnn_noresize, mask_rcnn_noresize, ssd_noresize, retinanet_noresize, ssd
+from rcnn import faster_rcnn_noresize, mask_rcnn_noresize, ssd_noresize, retinanet_noresize, ssd
 
-# Gated Transformer Import
-# from gtrxl_torch.gtrxl_torch import GTrXL
 
 
 #####################
